8f8872896557a1fd887c4627e41b9e6ed3f3692f
okada genya <g.okada.wilf+github@gmail.com>
2022-12-19 13:58:30 +0900

    fix: title

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Fresh App</title>
+        <title>シェアしたYoutubeを振り返ろう2022</title>
       </Head>
       <div class="p-4 mx-auto max-w-screen-md">
         <p class="my-6">
